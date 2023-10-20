6045d6dd01a1dec38ff14ccbe608cd95fe670f8c
= <=>
2023-10-21 00:33:56 +0200

    change nixpkgs to the stabel version

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "A simple NixOS flake";
   
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     home-manager = {
        url = "github:nix-community/home-manager";
        inputs.nixpkgs.follows = "nixpkgs";
     };
   }; 
 
-  outputs = { self, nixpkgs, home-manager ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
         system = "aarch64-linux";
         modules = [ ./configuration.nix ];
