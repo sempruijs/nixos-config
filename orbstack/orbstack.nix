85317684e0c374e4bcda9562f0de849c1d6ab993
sem pruijs <sem@pruijs.net>
2024-05-03 22:43:53 +0200

    update inputs

--- orbstack/orbstack.nix
+++ orbstack/orbstack.nix
@@ -43,14 +43,12 @@ with lib;
 
   # systemd
   systemd.services."systemd-oomd".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-resolved".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-userdbd".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-udevd".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-timesyncd".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-timedated".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-portabled".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-nspawn@".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-networkd".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-machined".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-localed".serviceConfig.WatchdogSec = 0;
   systemd.services."systemd-logind".serviceConfig.WatchdogSec = 0;
@@ -68,8 +66,10 @@ with lib;
   '';
 
   # extra certificates
-  security.pki.certificateFiles = [
-    "/opt/orbstack-guest/run/extra-certs.crt"
+  security.pki.certificates = [
+    ''
+
+    ''
   ];
 
   # indicate builder support for emulated architectures
