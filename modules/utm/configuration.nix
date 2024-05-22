90565195ba69b0d1319a8ac3e581aadf85075cf0
sem pruijs <sem@pruijs.net>
2024-05-22 11:21:15 +0200

    remove user definition utm configuration.nix

--- modules/utm/configuration.nix
+++ modules/utm/configuration.nix
@@ -73,16 +73,6 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.sem = {
-    isNormalUser = true;
-    description = "sem";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-    #  thunderbird
-    ];
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
