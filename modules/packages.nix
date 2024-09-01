180a1b5355a5e5707aa305e048a0024b126c4763
sem pruijs <sem@pruijs.net>
2024-09-01 17:56:44 +0200

    update input

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,7 +3,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    slides
     curl
     git
     gh
@@ -15,15 +14,13 @@
     tree
     bat
     bottom
-    gomuks
-    flyctl
     delta
     emacs
     nil
 
     inputs.helix.packages.aarch64-linux.default
     inputs.randaam-cli.packages.aarch64-linux.default
-    inputs.yazi.packages.aarch64-linux.default
+    # inputs.yazi.packages.aarch64-linux.default
 
     inputs.tools.packages.aarch64-linux.audio-from-yt
     inputs.tools.packages.aarch64-linux.to-chords
