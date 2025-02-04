21c5d80c4a5ca0862f30d0bd2ff2bdda036b2231
sem pruijs <sem@pruijs.net>
2025-02-04 14:02:54 +0100

    enable direnv fordarwin and orbstack, add home shortcut

--- hm/zsh.nix
+++ hm/zsh.nix
@@ -13,6 +13,7 @@
       pbcopyy = "xclip -selection c"; # macOS' pbcopy equivalent
       update-time = "sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\"";
       update = if platform == "darwin" then "darwin-rebuild switch --flake .#default" else "sudo nixos-rebuild switch --flake .#orbstack";
+      h = "cd /Users/sem";
     };
     initExtra =
       if platform == "darwin" then ''
@@ -21,11 +22,13 @@
           # No forwarded agent, use 1Password agent
           export SSH_AUTH_SOCK="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
         fi
+        eval "$(direnv hook zsh)"
       '' else ''
         # set the symlink that code-server/emacs uses to the most recently logged in $SSH_AUTH_SOCK
         if [ ! -h "$SSH_AUTH_SOCK" ]; then 
           ln -sfv "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock; 
         fi
+        eval "$(direnv hook zsh)"
       '';
   };
 }
